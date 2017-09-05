5f01a7dac11c2db2b396cfd7f2065cc4d61cc5f4
yuchen-lu <30705243+yuchen-lu@users.noreply.github.com>
2017-09-05 18:23:12 -0400

    Add files via upload

--- List & string.py
+++ List & string.py
@@ -1,4 +1,4 @@
-# sort list, split string, append list
+# -------sort list, split string, append list
 
 fname = input("Enter file name: ")
 fh = open(fname)
@@ -16,3 +16,26 @@ for line in fh:
 	
 read2.sort()
 print(read2)
+
+
+
+#------------print without string format, count, find keywords
+
+
+
+fname = input("Enter file name: ")
+if len(fname) < 1 : fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
+lst2=list()
+lst=list()
+for line in fh:
+	
+	if line.startswith('From '):
+		lst=line.split()
+		lst2.append(lst[1])
+		print(lst2[count])
+		count=count+1
+
+print("There were", count, "lines in the file with From as the first word")
